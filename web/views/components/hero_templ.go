e3e6ee838489e24f061db644f2ab6e38ccad5305
Ainsley Clark <34712954+ainsleyclark@users.noreply.github.com>
2026-05-08 11:19:47 +0100

    feat: Add styled error page with Vercel 404 support (#37)

--- web/views/components/hero_templ.go
+++ web/views/components/hero_templ.go
@@ -41,7 +41,7 @@ func Hero(flash string) templ.Component {
 			var templ_7745c5c3_Var2 string
 			templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(flash)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/hero.templ`, Line: 18, Col: 34}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/views/components/hero.templ`, Line: 18, Col: 34}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 			if templ_7745c5c3_Err != nil {
