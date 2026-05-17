edbd70beb5d437bcb057e1a1444dc888d06534fa
Ainsley Clark <hello@ainsley.dev>
2026-05-17 10:44:28 +0100

    refactor: Gen examples

--- web/views/pages/thankyou_templ.go
+++ web/views/pages/thankyou_templ.go
@@ -78,7 +78,7 @@ func ThankYou(email string) templ.Component {
 					var templ_7745c5c3_Var4 string
 					templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(email)
 					if templ_7745c5c3_Err != nil {
-						return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/pages/thankyou.templ`, Line: 19, Col: 82}
+						return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/views/pages/thankyou.templ`, Line: 19, Col: 82}
 					}
 					_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 					if templ_7745c5c3_Err != nil {
